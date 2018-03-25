0935f06ec3ac7adb23bbbabf3a36cc143eed022b
Khai Bui <khaibui@Khais-MacBook-Pro.local>
2018-03-24 17:55:54 -0700

    Add babel-polyfill to run async/await

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './public/index.js',
+  entry: ['babel-polyfill', './public/index.js'],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
