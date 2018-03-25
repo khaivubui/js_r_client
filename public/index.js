37060e2c57a0ca4f11fa392f4394f45408ee0667
Khai Bui <khaibui@Khais-MacBook-Pro.local>
2018-03-24 17:56:49 -0700

    Set up front end and back end to load wasm file

--- public/index.js
+++ public/index.js
@@ -1,6 +1,9 @@
-fetch('add.wasm')
-.then(response => response.arrayBuffer())
-.then(bytes => WebAssembly.instantiate(bytes, {}))
-.then(results => {
-    alert(results.instance.exports.add_one(41));
-});
+let rustNthFibonacci;
+
+const loadRustFibonacci = async () => {
+  const rustWASM = await WebAssembly.instantiateStreaming(fetch('js_r_client.wasm'), {});
+  rustNthFibonacci = rustWASM.instance.exports.nth_fibonacci;
+  window.rustNthFibonacci = rustNthFibonacci; // just for test
+};
+
+loadRustFibonacci();
--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/bundle.js', (req, res) => res.sendFile(path.join(__dirname, '../dist/bundle.js')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
+const BUNDLE_PATH = path.join(__dirname, '../dist/bundle.js');
+const WASM_PATH = path.join(
+  __dirname,
+  '../target/wasm32-unknown-unknown/release/js_r_client.wasm'
+);
+const HTML_PATH = path.join(__dirname, '../public/index.html');
+
+app
+  .get('/bundle.js', (req, res) => res.sendFile(BUNDLE_PATH))
+  .get('/js_r_client.wasm', (req, res) => res
+    .set('content-type', 'application/wasm')
+    .sendFile(WASM_PATH))
+  .get('*', (req, res) => res.sendFile(HTML_PATH));
 
 app.listen(
   port,
